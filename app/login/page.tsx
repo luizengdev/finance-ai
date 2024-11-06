0efd9aca0ee071a9f16040aba5308bddf46de509
luizengdev <luizengdev@gmail.com>
2024-11-06 09:34:24 -0300

    feat: update font to mulish

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -12,7 +12,7 @@ const LoginPage = async () => {
   }
 
   return (
-    <div className="grid h-full grid-cols-2">
+    <div className="grid h-full grid-cols-1 sm:grid-cols-2">
       {/* ESQUERDA */}
       <div className="mx-auto flex h-full max-w-[550px] flex-col justify-center p-8">
         <Image
@@ -23,7 +23,7 @@ const LoginPage = async () => {
           className="mb-8"
         />
         <h1 className="mb-3 text-4xl font-bold">Bem-vindo</h1>
-        <p className="text-muted-foreground mb-8">
+        <p className="mb-8 text-muted-foreground">
           A Finance AI é uma plataforma de gestão financeira que utilliza IA
           para monitorar suas movimentações, e oferecer insights personalizados,
           facillitando o controle do seu orçamento.
@@ -36,7 +36,7 @@ const LoginPage = async () => {
         </SignInButton>
       </div>
       {/* DIREITA */}
-      <div className="relative h-full w-full">
+      <div className="relative hidden sm:block sm:h-full sm:w-full">
         <Image
           src="/login.png"
           alt="Faça login"
