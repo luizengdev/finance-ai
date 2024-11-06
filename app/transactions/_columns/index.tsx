f306de6f3f0c911b046f16437c3eece1602a482d
luizengdev <luizengdev@gmail.com>
2024-11-06 16:05:43 -0300

    feat: ensure transaction table labels are rendered properly

--- app/transactions/_columns/index.tsx
+++ app/transactions/_columns/index.tsx
@@ -3,6 +3,12 @@
 import { Transaction } from "@prisma/client"
 import { ColumnDef } from "@tanstack/react-table"
 import TransactionTypeBadge from "../_components/type-badge"
+import {
+  TRANSACTION_CATEGORY_LABELS,
+  TRANSACTION_PAYMENT_METHOD_LABELS,
+} from "@/constants/transactions"
+import { Button } from "@/components/ui/button"
+import { PencilIcon, TrashIcon } from "lucide-react"
 
 export const transactionColumns: ColumnDef<Transaction>[] = [
   {
@@ -12,28 +18,53 @@ export const transactionColumns: ColumnDef<Transaction>[] = [
   {
     accessorKey: "type",
     header: "Tipo",
-    cell: ({ row: { original: transaction } }) => {
-      return <TransactionTypeBadge transaction={transaction} />
-    },
+    cell: ({ row: { original: transaction } }) => (
+      <TransactionTypeBadge transaction={transaction} />
+    ),
   },
   {
     accessorKey: "category",
     header: "Categoria",
+    cell: ({ row: { original: transaction } }) =>
+      TRANSACTION_CATEGORY_LABELS[transaction.category],
   },
   {
     accessorKey: "paymentMethod",
     header: "Método de Pagamento",
+    cell: ({ row: { original: transaction } }) =>
+      TRANSACTION_PAYMENT_METHOD_LABELS[transaction.paymentMethod],
   },
   {
     accessorKey: "date",
     header: "Data",
+    cell: ({ row: { original: transaction } }) =>
+      new Date(transaction.date).toLocaleDateString("pt-BR", {
+        day: "2-digit",
+        month: "long",
+        year: "numeric",
+      }),
   },
   {
     accessorKey: "amount",
     header: "Valor",
+    cell: ({ row: { original: transaction } }) =>
+      new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+      }).format(Number(transaction.amount)),
   },
   {
     accessorKey: "actions",
-    header: "",
+    header: "Ações",
+    cell: ({}) => (
+      <div className="space-x-1">
+        <Button variant="ghost" size="icon" className="text-muted-foreground">
+          <PencilIcon />
+        </Button>
+        <Button variant="ghost" size="icon" className="text-muted-foreground">
+          <TrashIcon />
+        </Button>
+      </div>
+    ),
   },
 ]
--- app/transactions/_components/type-badge.tsx
+++ app/transactions/_components/type-badge.tsx
@@ -1,31 +1,40 @@
 import { Badge } from "@/components/ui/badge"
 import { Transaction, TransactionType } from "@prisma/client"
 import { CircleIcon } from "lucide-react"
+import { TRANSACTION_TYPE_OPTIONS } from "@/constants/transactions"
 
 interface TransactionTypeBadgeProps {
   transaction: Transaction
 }
 
 const TransactionTypeBadge = ({ transaction }: TransactionTypeBadgeProps) => {
+  const typeOption = TRANSACTION_TYPE_OPTIONS.find(
+    (option) => option.value === transaction.type,
+  )
+
+  if (!typeOption) {
+    return null
+  }
+
   let badgeClass = ""
   let iconClass = ""
   let text = ""
 
-  switch (transaction.type) {
+  switch (typeOption.value) {
     case TransactionType.DEPOSIT:
       badgeClass = "bg-blue-800/80 text-blue-500 hover:bg-blue-800"
       iconClass = "fill-blue-500"
-      text = "Depósito"
+      text = typeOption.label
       break
     case TransactionType.EXPENSE:
       badgeClass = "bg-red-800/80 text-red-400 hover:bg-red-800"
       iconClass = "fill-red-400"
-      text = "Despesa"
+      text = typeOption.label
       break
     default:
       badgeClass = "bg-green-800/80 text-green-500 hover:bg-green-800"
       iconClass = "fill-green-500"
-      text = "Investimento"
+      text = typeOption.label
       break
   }
 
